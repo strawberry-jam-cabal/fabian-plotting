c9d3d713341f9d9a04eb46708ae3bb6ceece974e
Tobin Yehle <tobinyehle@gmail.com>
2022-10-23 11:26:46 -0600

    vectorized hair sketch

--- hair/sketch_hair.py
+++ hair/sketch_hair.py
@@ -1,9 +1,11 @@
 import math
 from random import random
+from typing import Tuple
 import vsketch
 from vsketch import Param, Vsketch
 import shapely.geometry as g
 import shapely.affinity as a
+import numpy as np
 
 
 def regular_polygon(sides: int) -> g.Polygon:
@@ -17,15 +19,28 @@ def grid(vsk: Vsketch, cols: int, rows: int, scale: float):
     return vsk.noise([x*scale for x in range(cols)], [y*scale for y in range(rows)])
 
 
-def hair(vsk, x, y, frequency, step_size, fuel=10) -> g.LineString:
-    result = [(x, y)]
+def hairs(vsk: Vsketch, x_starts: list[float], y_starts: list[float], frequency: float, step_size: float, fuel: int) -> list[g.LineString]:
+    xs = np.array(x_starts)
+    ys = np.array(y_starts)
+    points = [(xs, ys)]
     while fuel > 0:
-        fuel -=1
-        angle = vsk.noise(x*frequency, y*frequency) * 2 * math.pi
-        x += math.cos(angle)*step_size
-        y += math.sin(angle)*step_size
-        result.append((x, y))
-    return g.LineString(result)
+        fuel -= 1
+        (xs, ys) = steps(vsk, xs, ys, frequency, step_size)
+        points.append((xs, ys))
+    lines = []
+    for i in range(len(xs)):
+        coords = [(xs[i], ys[i]) for xs, ys in points]
+        if coords:
+            lines.append(g.LineString(coords))
+    return lines
+
+
+def steps(vsk: Vsketch, xs: np.ndarray, ys: np.ndarray, frequency: float, step_size: float) -> Tuple[np.ndarray, np.ndarray]:
+    angles = vsk.noise(xs*frequency, ys*frequency, grid_mode=False) * 2 * math.pi
+    new_xs = xs + np.cos(angles)*step_size
+    new_ys = ys + np.sin(angles)*step_size
+    return (new_xs, new_ys)
+
 
 
 class HairSketch(vsketch.SketchClass):
@@ -34,10 +49,11 @@ class HairSketch(vsketch.SketchClass):
     steps = Param(20)
     fuel = Param(20)
     detail = Param(0.25)
+    pen_width = Param(0.3)
 
     def draw(self, vsk: Vsketch) -> None:
         vsk.scale("cm")
-        vsk.penWidth(".1mm")
+        vsk.penWidth(f"{self.pen_width/3}mm")
 
         pixel_size = self.size / self.steps
 
@@ -58,16 +74,15 @@ class HairSketch(vsketch.SketchClass):
         # bounds = bounds.difference(g.Point(self.steps/2, self.steps/2).buffer(self.steps/3))
         # vsk.geometry(bounds)
         vsk.stroke(2)
-        starts = [complex(random()*(self.steps+6) - 3, random()*(self.steps+6) - 3) for _ in range((self.steps+6)**2)]
-        for c in starts:
-            x = c.real
-            y = c.imag
-            strand = hair(vsk, x, y, self.frequency, self.detail, fuel=self.fuel).intersection(bounds)
-            if strand.geom_type in ["LineString", "MultiLineString"]:
-                vsk.geometry(strand)
+        starts = [(random()*(self.steps+6) - 3, random()*(self.steps+6) - 3) for _ in range((self.steps+6)**2)]
+        strands = hairs(vsk, [x for x, _ in starts], [y for _, y in starts], self.frequency, self.detail, self.fuel)
+        for strand in strands:
+            clipped = strand.intersection(bounds)
+            if clipped.geom_type in ["LineString", "MultiLineString"]:
+                vsk.geometry(clipped)
 
         layout = f"layout 4.5inx6.25in"
-        pen = "penwidth .3mm color black color -l2 green"
+        pen = f"penwidth {self.pen_width}mm color black color -l2 green"
         vsk.vpype(f"{layout} {pen}")
 
     def finalize(self, vsk: Vsketch) -> None:
